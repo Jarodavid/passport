e4055a70b855f34b99041be44f8e13ec410be675
Jarodavid <40429691+Jarodavid@users.noreply.github.com>
2020-11-11 22:36:18 +0800

    .

--- server.js
+++ server.js
@@ -30,6 +30,10 @@ app.use(passport.initialize())
 app.use(passport.session())
 app.use(methodOverride('_method'))
 
+
+
+
+
 app.get('/', checkAuthenticated, (req, res) => {
   res.render('index.ejs', { name: req.user.name })
 })
